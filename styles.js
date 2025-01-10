b1e56616d79e39956039f1a2b637a316608a408d
ratihhhhhhhhh <rtih705@gmail.com>
2025-01-09 17:42:05 -0800

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,22 @@
+button {
+  background-color: #000;
+  color: #fff;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #444;
+}
+
+footer {
+  text-align: center;
+  background-color: #000;
+  color: #fff;
+  padding: 10px 0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
