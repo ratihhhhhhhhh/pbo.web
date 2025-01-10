4ecc75a969a72ed541548fb050978f8f809bd674
ratihhhhhhhhh <rtih705@gmail.com>
2025-01-09 17:44:16 -0800

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "Toko Sepatu",
+  "short_name": "Sepatu",
+  "description": "Toko sepatu online terbaik untuk berbagai jenis sepatu.",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "assets/images/icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "assets/images/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
